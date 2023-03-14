14d61cc1c0d29af0fe2836aec2c352a72de97305
afraz98 <anthonymfrazier1998@gmail.com>
2023-03-13 21:03:58 -0400

    Day 2 solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,7 +3,7 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 fn parse_readings(filename: &str) -> Vec<i64>{
-    let file = File::open("input.txt").unwrap();
+    let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
     let readings: Vec<i64> = reader.lines().map(|line| line.unwrap().parse::<i64>().unwrap()).collect();
     return readings;
@@ -24,11 +24,11 @@ fn count_increasing_measurements(readings: Vec<i64>) {
 
 pub fn main(){
     // Part 1
-    let readings = parse_readings("input.txt");
+    let readings = parse_readings("./input.txt");
     count_increasing_measurements(readings);
 
     // Part 2
-    let readings = parse_readings("input.txt");
+    let readings = parse_readings("./input.txt");
     let mut sums: Vec<i64> = Vec::new();
     for i in 0..readings.len() - 2 {
         sums.push(readings[i]+readings[i+1]+readings[i+2])
---
+++ day02/src/main.rs
@@ -0,0 +1,50 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn dive(filename: &str) {
+    let mut horiz = 0;
+    let mut depth = 0;
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line.unwrap().split_once(" ") {
+            Some(("up", value)) => depth = depth - value.parse::<i32>().unwrap(),
+            Some(("down", value)) => depth = depth + value.parse::<i32>().unwrap(),
+            Some(("forward", value)) => horiz = horiz + value.parse::<i32>().unwrap(),
+            _ => continue,
+        }
+    }
+
+    println!("{}", horiz * depth);
+}
+
+fn aim(filename: &str){
+    let mut horiz = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line.unwrap().split_once(" ") {
+            Some(("up", value)) => aim = aim - value.parse::<i32>().unwrap(),
+            Some(("down", value)) => aim = aim + value.parse::<i32>().unwrap(),
+            Some(("forward", value)) => {
+                horiz = horiz + value.parse::<i32>().unwrap();
+                depth += aim * value.parse::<i32>().unwrap();
+            }
+            _ => continue,
+        }
+    }
+
+    println!("{}", depth * horiz);
+}
+
+fn main() {
+    dive("input.txt");
+    aim("input.txt")
+}
